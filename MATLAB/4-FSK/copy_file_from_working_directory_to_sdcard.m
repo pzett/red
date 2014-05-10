41328a75f224affb21a81dc6ee7bf90b09ecf3ea
Francisc0 <frosario@kth.se>
2014-05-10 16:10:59 +0200

    system command strings...

--- MATLAB/4-FSK/copy_file_from_working_directory_to_sdcard.m
+++ MATLAB/4-FSK/copy_file_from_working_directory_to_sdcard.m
@@ -6,13 +6,13 @@ function copy_file_from_working_directory_to_sdcard( filename )
 %
 %
 % Copyright KTH Royal Institute of Technology, Martin Ohlsson, Per Zetterberg
-% This software is provided  ’as is’. It is free to use for non-commercial purposes.
-% For commercial purposes please contact Peter Händel (peter.handel@ee.kth.se)
+% This software is provided  ?as is?. It is free to use for non-commercial purposes.
+% For commercial purposes please contact Peter H?ndel (peter.handel@ee.kth.se)
 % for a license. For non-commercial use, we appreciate citations of our work,
 % please contact, Per Zetterberg (per.zetterberg@ee.kth.se), 
 % for how information on how to cite.
 
-
-   cmd_str=['C:\android\adt-bundle-windows-x86_64-20140321\sdk\platform-tools\adb push ',filename,' /sdcard/',filename];
+   cmd_str=['adb pull sdcard/',filename,' ',filename];
    system(cmd_str);
 end
+
--- MATLAB/4-FSK/get_log_data_from_FrameWork.m
+++ MATLAB/4-FSK/get_log_data_from_FrameWork.m
@@ -1,6 +1,4 @@
 
-
-
 function log_data = get_log_data_from_FrameWork( filename )
 %
 % This function parses the data in a sensorlog file and assigns it to a
@@ -14,8 +12,8 @@ function log_data = get_log_data_from_FrameWork( filename )
 %
 %
 % Copyright KTH Royal Institute of Technology, Martin Ohlsson, Per Zetterberg
-% This software is provided  ’as is’. It is free to use for non-commercial purposes.
-% For commercial purposes please contact Peter Händel (peter.handel@ee.kth.se)
+% This software is provided  ?as is?. It is free to use for non-commercial purposes.
+% For commercial purposes please contact Peter H?ndel (peter.handel@ee.kth.se)
 % for a license. For non-commercial use, we appreciate citations of our work,
 % please contact, Per Zetterberg (per.zetterberg@ee.kth.se), 
 % for how information on how to cite.

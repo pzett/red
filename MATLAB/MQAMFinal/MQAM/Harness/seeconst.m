dbc43e90af7280faa759dbbb6fd9d7130a364190
hefo <hefo@kth.se>
2014-05-28 15:48:23 +0200

    added seeconst to harness

---
+++ MATLAB/MQAMFinal/MQAM/Harness/seeconst.m
@@ -0,0 +1,32 @@
+system('adb devices')
+
+filename = 'demconst.txt';
+copy_file_from_sdcard(filename,'4df7721fd73fbfaf')
+close all
+
+
+fileID = fopen('demconst.txt','r');
+formatSpec = '%f';
+data = fscanf(fileID,formatSpec);
+L=data(1);
+mconst=zeros(L,1);
+for(k=2:L+1)
+    mconst(k-1) = data(k);
+end
+
+mreal=mconst(1:2:end-1);
+mimag=mconst(2:2:end);
+mconst=mreal+1i*mimag;
+
+scatterplot(mconst); grid on
+
+% fileID = fopen('rx_signal.txt','r');
+% formatSpec = '%f';
+% data = fscanf(fileID,formatSpec);
+% L=data(1);
+% receive=zeros(L,1);
+% for(k=2:L+1)
+%     receive(k-1) = data(k);
+% end
+% figure(2)
+% pwelch(receive);
